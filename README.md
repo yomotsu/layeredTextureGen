0e9f9e8864b225c94445d7e32cebb46cb3e60220
yomotsu <oyamada@pxgrid.com>
2015-05-02 21:53:04 +0900

    firstcommit

---
+++ README.md
@@ -0,0 +1,98 @@
+# layeredTextureGen
+
+One of progrem with WebGL for the web is loading huge textures. It causes long loading times. layeredTextureGen.js is the solution for that. layeredTextureGen.js can generate huge textures from small tile images and mask images in JavaScript.
+
+In this example, both tiles and masks are 256 x 256 pixel each. A totall of less than 200KB images generate a 2MB image.
+
+![](examples/readme.jpg)
+
+Then you can apply it to THREE.Mesh.
+
+![](examples/capture.jpg)
+
+## Live demos
+
+- [example1: basic usage](http://yomotsu.github.io/layeredTextureGen/examples/example1.html)
+- [example2](http://yomotsu.github.io/layeredTextureGen/examples/example2.html)
+
+## Usage
+
+Include both [three.js](https://github.com/mrdoob/three.js/) and layeredTextureGen.js
+```
+<script src="../lib/three.min.js"></script>
+<script src="../layeredTextureGen.js"></script>
+```
+
+Then, write JS code with three.js as usual. `layeredTextureGen.generate()` function will return a THREE.Texture instance. You can use it as texture with materials for three.js.
+
+```
+<script>
+
+var layer1 = new layeredTextureGen.Layer( {
+  tileURL: './images/grass1.jpg',
+  maskURL: './images/mask1.jpg',
+  tileRepeat: new THREE.Vector2( 12, 12 )
+} );
+
+var layer2 = new layeredTextureGen.Layer( {
+  tileURL: './images/grass2.jpg',
+  maskURL: './images/mask2.jpg',
+  tileRepeat: new THREE.Vector2( 12, 12 )
+} );
+
+var layer3 = new layeredTextureGen.Layer( {
+  tileURL: './images/sand.jpg',
+  maskURL: './images/mask3.jpg',
+  tileRepeat: new THREE.Vector2( 12, 12 )
+} );
+
+var layer4 = new layeredTextureGen.Layer( {
+  tileURL: './images/stone.jpg',
+  maskURL: './images/mask4.jpg',
+  tileRepeat: new THREE.Vector2( 12, 12 )
+} );
+
+var layeredTexture = layeredTextureGen.generate( 2048, 2048, [
+  layer1, // bottom
+  layer2,
+  layer3,
+  layer4  // top
+] );
+
+layeredTexture.addEventListener( 'loaded', function () {
+
+  console.log( 'generated texture was loaded!' );
+
+  // you can get the texture as an image like this, if you need
+  // document.body.appendChild( layeredTexture.image )
+
+} );
+
+
+var width  = window.innerWidth;
+var height = window.innerHeight;
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 60, width / height, 1, 1000 );
+camera.position.set( 0, 10, 10 );
+camera.lookAt( scene.position );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( width, height );
+document.body.appendChild( renderer.domElement );
+
+var plane = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry( 100, 100 ),
+  new THREE.MeshBasicMaterial( { map: layeredTexture } )
+);
+scene.add( plane );
+
+
+( function animate () {
+
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+
+} )();
+
+</script>
+```
---
+++ layeredTextureGen.js
@@ -0,0 +1,207 @@
+var layeredTextureGen = ( function () {
+
+  var layeredTextureGen = {};
+
+  var scene = new THREE.Scene();
+  var camera = new THREE.OrthographicCamera( -1, 1, 1, -1, 0, 1 );
+  var renderer = new THREE.WebGLRenderer( { preserveDrawingBuffer: true } );
+  var quad = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2, 2 ), null );
+  scene.add( quad );
+
+  var getVS = function () {
+
+    var vs = [
+      'varying vec2 vUv;',
+
+      'void main () {',
+
+        'vUv = uv;',
+        'gl_Position =   projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+
+      '}'
+    ].join( '\n' );
+
+    return vs;
+
+  };
+
+
+  var getFS = function ( num ) {
+
+    var uniform = [];
+
+    for ( i = 0; i < num; i ++ ) {
+
+      uniform.push(
+        'uniform sampler2D layer' + i + ';',
+        'uniform vec2 repeat'     + i + ';',
+        'uniform sampler2D mask'  + i + ';'
+      );
+
+    }
+
+    var alpha = [];
+
+    for ( i = 0; i < num; i ++ ) {
+
+      alpha.push(
+        'float alpha' + i + ' = texture2D( mask' + i + ', vUv ).x;'
+      );
+
+    }
+
+    var color = [];
+
+    for ( i = 0; i < num; i ++ ) {
+
+      color.push(
+        'vec3 color' + i + ' = texture2D( layer' + i + ', vUv * repeat' + i + ' ).xyz;'
+      );
+
+    }
+
+    var diffuse = [];
+
+    for ( i = 0; i < num; i ++ ) {
+
+      diffuse.push(
+        'diffuse = alphaBlend( vec4( color' + i + ', alpha' + i + ' ), diffuse );'
+      );
+
+    }
+
+
+    var fs = [
+      'precision highp float;',
+
+      'varying vec2 vUv;',
+
+      uniform.join( '\n' ),
+
+      'vec4 alphaBlend ( vec4 src, vec4 dst ) {',
+
+        'vec3 color = vec3( src.xyz ) * src.w + vec3( dst.xyz ) * ( 1. - src.w );',
+        'return vec4( color, 1 );',
+
+      '}',
+
+      'void main(void) {',
+
+        'vec4 diffuse = vec4( 0, 0, 0, 1 );',
+
+        alpha.join( '\n' ),
+        color.join( '\n' ),
+        diffuse.join( '\n' ),
+
+        'gl_FragColor = diffuse;',
+
+      '}'
+    ].join( '\n' );
+
+    return fs;
+
+  };
+
+  layeredTextureGen.Layer = function ( param ) {
+
+    THREE.EventDispatcher.prototype.apply( this );
+
+    var that = this;
+    var count = 0;
+
+    this.tile = THREE.ImageUtils.loadTexture(
+      param.tileURL,
+      {},
+      onload
+    );
+    this.tile.wrapS = THREE.RepeatWrapping;
+    this.tile.wrapT = THREE.RepeatWrapping;
+    this.mask = THREE.ImageUtils.loadTexture(
+      param.maskURL,
+      {},
+      onload
+    );
+    this.tileRepeat = param.tileRepeat || new THREE.Vector2( 1, 1 );
+
+    function onload () {
+
+      count ++;
+
+      if ( count >= 2 ) {
+
+        that.dispatchEvent( { type: 'loaded' } );
+
+      }
+
+    };
+
+  };
+
+
+  layeredTextureGen.generate = function ( width, height, layerArray ) {
+
+    var count = 0;
+    var image = new Image();
+    var texture = new THREE.Texture( image );
+
+    var uniforms = {};
+
+    for ( var i = 0, l = layerArray.length; i < l; i ++ ) {
+
+      uniforms[ 'layer'  + i ] = { type: 't', value: null };
+      uniforms[ 'mask'   + i ] = { type: 't', value: null };
+      uniforms[ 'repeat' + i ] = { type: 'v2', value: new THREE.Vector2( 1, 1 ) };
+
+    }
+
+    var material = new THREE.ShaderMaterial( {
+      vertexShader: getVS(),
+      fragmentShader: getFS( layerArray.length ),
+      uniforms: uniforms
+    } );
+
+    for ( var i = 0, l = layerArray.length; i < l; i ++ ) {
+
+      material.uniforms[ 'layer'  + i ].value = layerArray[ i ].tile;
+      material.uniforms[ 'mask'   + i ].value = layerArray[ i ].mask;
+      material.uniforms[ 'repeat' + i ].value.copy( layerArray[ i ].tileRepeat );
+
+      layerArray[ i ].addEventListener( 'loaded', function () {
+
+        quad.material = material;
+        material.needsUpdate = true;
+        renderer.setSize( width, height );
+        renderer.render( scene, camera );
+
+        image.src = renderer.domElement.toDataURL( 'image/png' );
+        texture.needsUpdate = true;
+
+        count ++;
+
+        if ( count >= layerArray.length ) {
+
+          texture.dispatchEvent( { type: 'loaded' } );
+          // TODO
+          // material should be removed here
+
+        }
+
+      } );
+
+    }
+
+    return texture;
+
+  }
+
+  layeredTextureGen.clear = function () {
+
+    // TODO
+    // material.dispose();
+    // texture.dispose();
+
+  }
+
+  return layeredTextureGen;
+
+} )();
